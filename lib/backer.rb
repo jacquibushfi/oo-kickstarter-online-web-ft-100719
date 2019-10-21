d9a45641d1c0b42e3acec9129f2eb703035bb14f
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 14:28:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   end
 
   def back_project(project)
-    @backed_project << project
+    @backed_projects << project
      project.add_backer(self) unless project.backers.include?(self)
   end
 
